e71aeb6e4a28fd0275e813892950cc0404c8274f
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2021-12-20 19:53:39 +0330

    Updated contact page

--- portfolio - Copy/js/app.js
+++ portfolio - Copy/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   //Variable
   let navItmLnks = $(".nav .nav-item .nav-link");
   let sections = $("section");
+  let emailRegEx = /[0-9a-zA-Z]+\@+[a-z]+\.+['com']/;
   let observer = new MutationObserver((mutations) => {
     for (let mutation of mutations) {
       // examine new nodes, is there anything to highlight?
@@ -11,16 +12,16 @@ $(document).ready(function () {
         if (!(node instanceof HTMLElement)) continue;
 
         // check the inserted element for being a code snippet
-        if (node.matches('.work')) {
-          $(node).find('.zoom-it').on('click', deWorkZomItClckHandler);
+        if (node.matches(".work")) {
+          $(node).find(".zoom-it").on("click", deWorkZomItClckHandler);
         }
       }
     }
   });
+  let works = document.querySelector("#works .row");
+  let x, y;
 
-  let demoElem = document.querySelector("#works .row");
-
-  observer.observe(demoElem, { childList: true, subtree: true });
+  observer.observe(works, { childList: true, subtree: true });
 
   //Event Listener
   $(window).on("load", dtctTrgtByHash).on("load", addWorks);
@@ -30,6 +31,7 @@ $(document).ready(function () {
   $(".section-control .prev-section").on("click", goPrevSctcn);
   $(".section-control .next-section").on("click", goNxtSctcn);
   $(document).on("keydown", deKyPrsHandler);
+  $("#contact form").on("submit", deContctFrmSbmtHandler);
 
   //Event Handler
   function dtctTrgtByHash() {
@@ -54,9 +56,19 @@ $(document).ready(function () {
   function deWorkZomItClckHandler() {
     event.preventDefault();
     $("#galleryModal").removeClass("d-none");
-    $('#gallery .carousel-inner .carousel-item').removeClass('active');
-    console.log($(`#gallery .carousel-inner .carousel-item[data-order="${$(this).data('order')}"`));
-    $(`#gallery .carousel-inner .carousel-item[data-order="${$(this).data('order')}"`).addClass('active');
+    $("#gallery .carousel-inner .carousel-item").removeClass("active");
+    console.log(
+      $(
+        `#gallery .carousel-inner .carousel-item[data-order="${$(this).data(
+          "order"
+        )}"`
+      )
+    );
+    $(
+      `#gallery .carousel-inner .carousel-item[data-order="${$(this).data(
+        "order"
+      )}"`
+    ).addClass("active");
   }
 
   function deWorkDtilsClckHandler() {
@@ -64,6 +76,37 @@ $(document).ready(function () {
     event.preventDefault();
   }
 
+  function deContctFrmSbmtHandler() {
+    event.preventDefault();
+    $("#contact form .alert-danger").addClass("d-block");
+    if ($("#email").val() === "") {
+      // If email is not filled
+      $("#contact form .alert-danger")
+        .html("لطفا ایمیل خود را وارد کنید")
+        .removeClass("d-none");
+    } else if (!emailRegEx.test($("#email").val())) {
+      // If email is invalid
+      $("#contact form .alert-danger")
+        .html("لطفا یک ایمیل معتبر وارد کنید")
+        .removeClass("d-none");
+    } else if (Number($("#cap").val()) !== x + y) {
+      // If recaptcha is invalid
+      $("#contact form .alert-danger")
+        .html("اشتباه! لطفا محاسبه را دوباره انجام دهید")
+        .removeClass("d-none");
+      gnrtCptcha();
+    } else {
+      let value = new FormData(document.getElementById("contactForm")); // Make the value to send to server
+      sndRqstoSrvr("send_mail/", value); // Send the form data
+    }
+  }
+
+  function deKyPrsHandler() {
+    if (event.keyCode == 39) goPrevSctcn(); // Go to previous page by pressing arrow right
+    if (event.keyCode == 37) goNxtSctcn(); // Go to previous page by pressing arrow left
+  }
+
+  //Function
   function goPrevSctcn() {
     //Store the current nav item
     let current = $("header .nav .active");
@@ -74,12 +117,6 @@ $(document).ready(function () {
       navItmLnks[navItmLnks.length - 1].click();
     }
   }
-
-  function deKyPrsHandler() {
-    if (event.keyCode == 39) goPrevSctcn();
-    if (event.keyCode == 37) goNxtSctcn();
-  }
-
   function goNxtSctcn() {
     //Store the current nav item
     let current = $("header .nav .active");
@@ -91,7 +128,6 @@ $(document).ready(function () {
     }
   }
 
-  //Function
   function wrtMe() {
     $(".about-txt")
       .typeIt("مشاور و مربی کسب‌وکار", 0.3)
@@ -124,7 +160,9 @@ function addWorks() {
                 <img src="img/Projects/${el.imageUrl}">
                 <div class="layer">
                     <div class="w-100 d-flex justify-content-center">
-                        <a href="https://1353.ir/${el.url == "1353" ? "" : el.url}" class="details mx-3 bg-white text-decoration-none rounded">
+                        <a href="https://1353.ir/${
+                          el.url == "1353" ? "" : el.url
+                        }" class="details mx-3 bg-white text-decoration-none rounded">
                             <i class="fas fa-link"></i>
                         </a>
                         <a href="#x" class="zoom-it mx-3 bg-white text-decoration-none rounded" data-order="${i}">
@@ -146,9 +184,12 @@ function addWorks() {
       `;
 
       $("#works .row, #blog .row").append(work);
-      $('#gallery .carousel-inner').append(img);
+      $("#gallery .carousel-inner").append(img);
     });
   });
+
+  // Calls
+  gnrtCptcha();
 }
 
 async function loadJson(url) {
@@ -156,4 +197,40 @@ async function loadJson(url) {
   let json = await res.json();
 
   return json;
+}
+
+// Generates the captcha code
+function gnrtCptcha() {
+  x = Math.floor(Math.random() * 49) + 1;
+  y = Math.floor(Math.random() * 49) + 1;
+
+  let canvas = document.querySelector("#contact form .captcha canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "16px Arial";
+  ctx.direction = "ltr";
+  ctx.fillText(`${x} + ${y} =`, 5, 30);
+}
+
+function sndRqstoSrvr(url, value){ // Sends request and receives result from server
+  let xhtp; // To hold the object
+  if (window.XMLHttpRequest){
+      xhtp = new XMLHttpRequest(); // For modern browsers
+  }else{
+      xhtp = window.ActiveXObject('Microsoft.XMLHTPP'); // For old ones
+  }
+
+  xhtp.open('POST', url, true); // Opens the request
+  xhtp.send(value);
+
+  xhtp.addEventListener('readystatechange', () => {
+      if (xhtp.readyState == 4 && xhtp.status == 200){
+          if (xhtp.responseText == 'success') {
+              $('#contact form .alert-success').html('ایمیل با موفقیت ارسال شد').addClass('d-block');
+          }else{
+              $('#contact form .alert-danger').html('متاسفانه خطایی رخ داده است، لطفا دوباره تلاش کنید').addClass('d-block');
+          }
+          console.log(xhtp.responseText);
+      }
+  });
 }
